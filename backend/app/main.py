47f2dbb82a012023c975082ab3ac04a70077f24d
Imran Yafith <imranyafith@gmail.com>
2026-03-07 17:25:21 -0500

    feat(backend): summarize endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from app.routers.agreements import router as agreement_router
 
 from app.db import create_db_and_tables
 
 load_dotenv()
 
 app = FastAPI()
+app.include_router(agreement_router)
 
 
 @app.on_event("startup")
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,27 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 
 
-class Tolerances(SQLModel, table=True):
+class Question(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    user_id: str = Field(index=True)
+    text: str = Field(unique=True, index=True)
+
+    summaries: list["Summary"] = Relationship(back_populates="question")
+
+
+class Agreement(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    text: str = Field(unique=True, index=True)
+
+    summaries: list["Summary"] = Relationship(back_populates="agreement")
+
+
+class Summary(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    question_id: int = Field(foreign_key="question.id")
+    agreement_id: int = Field(foreign_key="agreement.id")
+
+    summary_text: str
+
+    question: Question = Relationship(back_populates="summaries")
+    agreement: Agreement = Relationship(back_populates="summaries")
